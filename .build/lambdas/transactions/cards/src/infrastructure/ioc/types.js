cc187a343a81fa2052f0bf7296fa0c05b55de7c0
Joshua <jovillenad@gmail.com>
2023-10-09 07:23:50 -0500

    Delete .build directory

--- .build/lambdas/transactions/cards/src/infrastructure/ioc/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Types = void 0;
-exports.Types = {
-    CardsDAO: Symbol.for("CardsDAO"),
-    CreateToken: Symbol.for("CreateToken"),
-    GetInfo: Symbol.for("GetInfo")
-};
-//# sourceMappingURL=types.js.map
--- .build/lambdas/transactions/cards/src/infrastructure/validators/token.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenSchema = void 0;
-const Joi = require("joi");
-exports.tokenSchema = Joi.object({
-    token: Joi.string().min(16).max(16).required(),
-}).required();
-//# sourceMappingURL=token.schema.js.map
